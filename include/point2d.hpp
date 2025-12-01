436407fca9a216cbabdc7e14b04fdc23b5ebf59c
Aloïs Guillon <alois.guillon@eleves.ec-nantes.fr>
2025-12-01 15:53:06 +0100

    Ajout classe Point2D

--- include/point2d.hpp
+++ include/point2d.hpp
@@ -13,9 +13,79 @@
  * 
  */
 
+#ifndef point2d_hpp
+#define point2d_hpp
+
 #pragma once
 
+#include <iostream>
+using namespace std;
+
 template <typename T>
-class point2d {
-    
+std::ostream& operator<<(std::ostream &, Point2D<T> const&);
+
+template <typename T>
+class Point2D {
+	protected:
+		T x;
+		T y;
+	public:
+		Point2D(T x, T y);
+		Point2D(Point2D const & point2d);
+		Point2D();
+		T getx();
+		T gety();
+		void setx(T x);
+		void sety(T y);
+		void translater(T a, T b);
+};
+
+template <typename T>
+Point2D<T>::Point2D(T x, T y){
+	this->x = x;
+	this->y = y;
 }
+
+template <typename T>
+Point2D<T>::Point2D(Point2D const & point2d){
+	this->x = x->point2d;
+	this->y = y->point2d;
+}
+
+template <typename T>
+Point2D<T>::Point2D(){
+	this->x = static_cast<T>(0);
+	this->y = static_cast<T>(0);
+}
+
+template <typename T>
+T Point2D<T>::getx(){
+	return this->x;
+}
+
+template <typename T>
+T Point2D<T>::gety(){
+	return this->y;
+}
+
+template <typename T>
+void Point2D<T>::setx(T x){
+	this->x = x;
+}
+
+template <typename T>
+void Point2D<T>::sety(T y){
+	this->y = y;
+}
+
+template <typename T>
+void Point2D<T>::translater(T a, T b){
+	this->x += a;
+	this->y += b;
+	
+friend std::ostream& operator<< <T>(std::ostream &, Point2D const&);
+
+}
+
+#endif
+
