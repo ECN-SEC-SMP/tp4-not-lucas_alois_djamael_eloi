f1aaa6b25c4a43f3b92d8ce8f4e6d55ff26e0788
Eloi Tourangin <eloi.tourangin@eleves.ec-nantes.fr>
2025-12-01 17:37:26 +0100

    Prepare implementation of carte.

--- include/carte.hpp
+++ include/carte.hpp
@@ -4,11 +4,49 @@
  * @author Lucas Oros (lucas.oros@eleves.ec-nantes.fr)
  * @author Djamael Lahamion (djamael.lahamion@eleves.ec-nantes.fr)
  * @author Aloïs Guillon (alois.guillon@eleves.ec-nantes.fr)
- * 
- * @brief 
+ *
+ * @brief
  * @version 0.1
  * @date 01-12-2025
- * 
+ *
  * @copyright Copyright (c) 2025
- * 
+ *
  */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "parcelle.hpp"
+#include "za.hpp"
+#include "zau.hpp"
+#include "zn.hpp"
+#include "zu.hpp"
+
+using namespace std;
+
+class carte
+{
+private:
+    vector<Parcelle *> listeParcelles;
+
+    float surfaceTotale;
+
+public:
+    /**
+     * @brief Construct a new carte object
+     *
+     * @param pathToFile
+     */
+    carte(string pathToFile);
+
+    ~carte();
+
+    /**
+     * @brief Méthode permettant de sauvegarder la carte dans un fichier
+     * 
+     * @param pathToFile chemin du fichier de sauvegarde
+     */
+    void sauvegarder(string pathToFile);
+};
--- src/carte.cpp
+++ src/carte.cpp
@@ -4,11 +4,31 @@
  * @author Lucas Oros (lucas.oros@eleves.ec-nantes.fr)
  * @author Djamael Lahamion (djamael.lahamion@eleves.ec-nantes.fr)
  * @author Aloïs Guillon (alois.guillon@eleves.ec-nantes.fr)
- * 
- * @brief 
+ *
+ * @brief
  * @version 0.1
  * @date 01-12-2025
- * 
+ *
  * @copyright Copyright (c) 2025
- * 
+ *
  */
+
+#include "carte.hpp"
+
+carte::carte(string pathToFile)
+{
+    // TODO
+}
+
+carte::~carte()
+{
+    for (Parcelle *parcelle : listeParcelles)
+    {
+        delete parcelle;
+    }
+}
+
+void carte::sauvegarder(string pathToFile)
+{
+    // TODO
+}
